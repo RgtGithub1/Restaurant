eb8312937201378522e82fb2cc09b049df41fe4a
RgtGithub1 <134707020+RgtGithub1@users.noreply.github.com>
2023-06-12 16:23:40 +0530

    Delete restaurant_project directory
    no use

--- restaurant_project/menu/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'menu'
-urlpatterns = [
-    path('', views.menu_list, name='menu_list'),
-    path('<int:id>/<slug:menu_slug>/', views.food_items_details, name='food_list_by_category'),
-    ]
